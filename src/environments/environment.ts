b1e270febc15d2c6eb71a152fe1c0b608db0b76b
frazaovitor <51264703+frazaovitor@users.noreply.github.com>
2020-11-07 18:17:10 -0300

    Conectando com o firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAdPwQsA0saA8d8uuEydFbhBCXcPDdEb58",
+    authDomain: "senacapp-e9af0.firebaseapp.com",
+    databaseURL: "https://senacapp-e9af0.firebaseio.com",
+    projectId: "senacapp-e9af0",
+    storageBucket: "senacapp-e9af0.appspot.com",
+    messagingSenderId: "429456932058",
+    appId: "1:429456932058:web:1858050c5a26bf67a179e3"
+  }
 };
 
 /*
