b81c8c6818b34659196f17bf371fac352ad16c89
frazaovitor <51264703+frazaovitor@users.noreply.github.com>
2020-11-07 18:05:33 -0300

    Primeiros Ajustes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,34 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  
+  {
+    path: 'info',
+    loadChildren: () => import('./pages/info/info.module').then( m => m.InfoPageModule)
+  },
+  {
+    path: 'contato',
+    loadChildren: () => import('./pages/contato/contato.module').then( m => m.ContatoPageModule)
+  },
+  {
+    path: 'test01',
+    loadChildren: () => import('./pages/test01/test01.module').then( m => m.Test01PageModule)
+  },
+  {
+    path: 'test02',
+    loadChildren: () => import('./pages/test02/test02.module').then( m => m.Test02PageModule)
+  },
+  {
+    path: 'e404',
+    loadChildren: () => import('./pages/e404/e404.module').then( m => m.E404PageModule)
+  },
 ];
 
 @NgModule({
